21983db6e10c52a1a5c8198c3c36978ed597ec9d
StaicuBogdan <61222625+StaicuBogdan@users.noreply.github.com>
2020-05-11 22:50:35 +0300

    Add files via upload

---
+++ Tema_POO_1/cmake-build-debug/CMakeFiles/Tema_POO_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tema_POO_1.dir/main.cpp.obj"
+  "Tema_POO_1.exe"
+  "Tema_POO_1.exe.manifest"
+  "Tema_POO_1.pdb"
+  "libTema_POO_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tema_POO_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
