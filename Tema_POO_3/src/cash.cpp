c19d52217366f2f2901df17f57a08af70e90c8f1
StaicuBogdan <61222625+StaicuBogdan@users.noreply.github.com>
2020-05-23 21:02:53 +0300

    updated part of code

--- Tema_POO_3/src/cash.cpp
+++ Tema_POO_3/src/cash.cpp
@@ -1,11 +1,28 @@
 #include "cash.h"
+#include <iostream>
 
-cash::cash()
-{
-    //ctor
+cash::cash(string data_val)
+    :tranzactie(data_val){
+        //cout<<"Cash C"<<endl;
 }
 
-cash::~cash()
-{
-    //dtor
+cash::~cash(){
+}
+
+int cash::get_suma() const{
+    return suma;
+}
+
+string cash::get_data() const{
+    return data;
+}
+
+void cash::afiseazaTranzactie(){
+    cout<<"Suma: "<<suma<<" "<<"Data: "<<data<<endl;
+}
+
+istream &operator>>(istream &input, cash &c){
+    tranzactie *temp= &c;
+    input >> *temp;
+    return input;
 }
