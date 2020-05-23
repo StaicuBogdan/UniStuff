e3c832abfd1a517292ea0ea20dfd496c2953254a
StaicuBogdan <61222625+StaicuBogdan@users.noreply.github.com>
2020-05-23 20:58:49 +0300

    updated part of code

--- Tema_POO_3/include/tranzactie.h
+++ Tema_POO_3/include/tranzactie.h
@@ -1,16 +1,25 @@
 #ifndef TRANZACTIE_H
 #define TRANZACTIE_H
+#include <string>
 
+using namespace std;
 
-class tranzactie
-{
+class tranzactie{
+    protected:
+        static int nrTranzactii;
+        int suma;
+        string data;
     public:
-        tranzactie();
+        tranzactie(string data_val="");
         virtual ~tranzactie();
 
-    protected:
+        virtual int get_suma() const;
+        virtual string get_data() const;
+        virtual void afiseazaTranzactie();
+        static int get_nr_tranzactii();
+        void increase_nr_tranzactii();
 
-    private:
+        friend istream &operator>>(istream &input, tranzactie &t);
 };
 
 #endif // TRANZACTIE_H
