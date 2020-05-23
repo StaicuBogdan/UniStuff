195b3622246f4053e25c9c2b9257d77ca3f5b745
StaicuBogdan <61222625+StaicuBogdan@users.noreply.github.com>
2020-05-23 21:02:03 +0300

    updated part of code

--- Tema_POO_3/src/cec.cpp
+++ Tema_POO_3/src/cec.cpp
@@ -1,11 +1,30 @@
 #include "cec.h"
+#include <iostream>
 
-cec::cec()
-{
-    //ctor
+cec::cec(string data_val)
+    :tranzactie(data_val){
+        //cout<<"Cec C"<<endl;
 }
 
-cec::~cec()
-{
-    //dtor
+cec::~cec(){
+}
+
+int cec::get_suma() const{
+    return suma;
+}
+
+string cec::get_data() const{
+    return data;
+}
+
+void cec::afiseazaTranzactie(){
+    cout<<"Suma: "<<suma<<" Numele: "<<nume<<" Data: "<<data<<endl;
+}
+
+istream &operator>>(istream &input, cec &ce){
+    tranzactie *temp= &ce;
+    input >> *temp;
+    cout<<"Numele dumneavoastra: ";
+    input>>ce.nume;
+    return input;
 }
