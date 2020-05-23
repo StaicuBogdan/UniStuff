50825a519d17f54a253eeeb0035ead7f292eb620
StaicuBogdan <61222625+StaicuBogdan@users.noreply.github.com>
2020-05-23 21:02:22 +0300

    updated part of code

--- Tema_POO_3/src/card.cpp
+++ Tema_POO_3/src/card.cpp
@@ -1,11 +1,32 @@
 #include "card.h"
+#include<iostream>
 
-card::card()
-{
-    //ctor
+card::card(string data_val)
+    :tranzactie(data_val){
+        //cout<<"Card C"<<endl;
 }
 
-card::~card()
-{
-    //dtor
+card::~card(){
+}
+
+int card::get_suma() const{
+    return suma;
+}
+
+string card::get_data() const{
+    return data;
+}
+
+void card::afiseazaTranzactie(){
+    cout<<"Suma: "<<suma<<" Numele: "<<nume<<" Nr card: "<<nrCard<<" Data: "<<data<<endl;
+}
+
+istream &operator>>(istream &input, card &ca){
+    tranzactie *temp= &ca;
+    input>>*temp;
+    cout<<"Numele dumneavoastra: ";
+    input>>ca.nume;
+    cout<<"Numarul cardului: ";
+    input>>ca.nrCard;
+    return input;
 }
