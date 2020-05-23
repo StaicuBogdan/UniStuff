6c658b6c5085bbae2f026ff12349946ab70489c1
StaicuBogdan <61222625+StaicuBogdan@users.noreply.github.com>
2020-05-23 21:01:30 +0300

    updated part of code

--- Tema_POO_3/src/tranzactie.cpp
+++ Tema_POO_3/src/tranzactie.cpp
@@ -1,11 +1,40 @@
 #include "tranzactie.h"
+#include <iostream>
 
-tranzactie::tranzactie()
-{
-    //ctor
+using namespace std;
+
+int tranzactie::nrTranzactii=0;
+
+tranzactie::tranzactie(string data_val)
+    :data{data_val}{
+        //cout<<"Tranzactie C"<<endl;
 }
 
-tranzactie::~tranzactie()
-{
-    //dtor
+tranzactie::~tranzactie(){
 }
+
+int tranzactie::get_suma() const{
+    return suma;
+}
+
+string tranzactie::get_data() const{
+    return data;
+}
+
+void tranzactie::afiseazaTranzactie(){
+    cout<<"Suma: "<<suma<<" "<<"Data: "<<data<<endl;
+}
+
+int tranzactie::get_nr_tranzactii(){
+    return nrTranzactii;
+}
+
+void tranzactie::increase_nr_tranzactii(){
+    nrTranzactii++;
+}
+
+istream &operator >> (istream &input, tranzactie &t){
+    cout<<"Introduceti suma: ";
+    cin>>t.suma;
+}
+
